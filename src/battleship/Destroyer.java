0850ef43158417c0d96d78842de2cf9c3adf9efa
lethangomes <35245543+lethangomes@users.noreply.github.com>
2022-05-25 12:23:11 -0700

    User can add other boats now
    Board now checks for ships intersecting but only for east facing boats

---
+++ src/battleship/Destroyer.java
@@ -0,0 +1,8 @@
+package battleship;
+
+public class Destroyer extends Ship {
+	public Destroyer(int newX, int newY, int newOrientation){
+		super(newX, newY, newOrientation);
+		length = 3;
+	}
+}
---
+++ src/battleship/PatrolBoat.java
@@ -0,0 +1,8 @@
+package battleship;
+
+public class PatrolBoat extends Ship {
+	public PatrolBoat(int newX, int newY, int newOrientation){
+		super(newX, newY, newOrientation);
+		length = 2;
+	}
+}
